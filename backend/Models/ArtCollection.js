ca3763cc7ae67b5ad576e7b23c4070cc41dd9612
Daniel Yuki <danielyukihiga1@gmail.com>
2024-07-23 19:18:53 +0900

    add collection address

--- backend/Models/ArtCollection.js
+++ backend/Models/ArtCollection.js
@@ -1,6 +1,7 @@
 class ArtCollection {
-    constructor( ArtCollectionId, creatorId, creatorName, collectionName, collectionCoverUrl, description, createdAt, price, isFree) {
+    constructor( ArtCollectionId, ArtCollectionAddress, creatorId, creatorName, collectionName, collectionCoverUrl, description, createdAt, price, isFree) {
         (this.ArtCollectionId = ArtCollectionId),
+        (this.ArtCollectionAddress = ArtCollectionAddress),
         (this.creatorId = creatorId),
         (this.creatorName = creatorName),
         (this.collectionName = collectionName),
