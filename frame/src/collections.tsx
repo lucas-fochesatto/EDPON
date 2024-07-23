1ba11dd6039b2740a9d997ec94c0de14d7921df7
Arthur-Jacobina <email27arthur@gmail.com>
2024-07-23 11:52:45 +0900

    dynamic routes to collections' list

--- frame/src/collections.tsx
+++ frame/src/collections.tsx
@@ -1,8 +1,10 @@
 import { Button, Frog } from 'frog';
-import { Box, Heading, Text, VStack, Image, vars } from "../lib/ui.js";
+import { vars } from "../lib/ui.js";
 
 const title = 'edpon collections';
 
+const collectionNames = ['Milady', 'Col2', 'Col3', 'Col4'];
+const artistNames = ['Remilia', 'Art2', 'Art3', 'Art4'];
 export const collectionsApp = new Frog({
   title,
   assetsPath: '/',
@@ -10,7 +12,10 @@ export const collectionsApp = new Frog({
   ui: { vars },
 })
 
-collectionsApp.frame('/', (c) => {
+collectionsApp.frame('/:id', (c) => {
+  const index = Number(c.req.param('id'));
+  const collectionName = collectionNames[(index%collectionNames.length)];
+  const artistName = artistNames[(index%collectionNames.length)]; 
   return c.res({
     title,
     image: (
@@ -24,29 +29,32 @@ collectionsApp.frame('/', (c) => {
             justifyContent: 'center',
             backgroundImage: "url(https://i.imgur.com/IcfnuQ0.png)",
             fontSize: 60,
+            backgroundGrow: 'true',
             backgroundSize: "cover",
             // backgroundWidth:"100%",
-            backgroundPosition: "top center",
+            backgroundPosition: 'top center',
             height: "100%",
             width:"100%",
             backgroundRepeat: 'no-repeat',
+            margin: 0,
+            padding: 32,
           }}
         >
           <p style={{
             margin: 0,
-          }}>Collection Name</p>
+          }}>{collectionName}</p>
           <p style={{
             color: 'white',
             fontSize: 30,
             margin: 0,
           }}
-          >Artist Name</p>
+          >{artistName}</p>
       </div>
     ),
     imageAspectRatio: '1:1',
     intents: [
-      <Button action='/'>⬅️</Button>,
-      <Button action='/'>➡️</Button>, 
+      <Button action={`/${index===0?(collectionNames.length-1):(((index-1)%collectionNames.length))}`}>⬅️</Button>,
+      <Button action={`/${((index+1)%collectionNames.length)}`}>➡️</Button>, 
       <Button action='/'>Pick! ✅</Button>, 
       <Button.Reset>Reset</Button.Reset>,
     ],
