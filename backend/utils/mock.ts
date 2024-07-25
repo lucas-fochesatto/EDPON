087724e742d4a5c19ea1765ff867bf046cca18b3
Daniel Yuki <danielyukihiga1@gmail.com>
2024-07-26 03:01:06 +0900

    quick fix + createdByEDPON

---
+++ backend/utils/mock.ts
@@ -0,0 +1,19 @@
+const mockCollection = {
+	artCollectionAddress: "0x0",
+	creatorId: "HAVETOPASSCREATORID",
+	collectionCoverUrl: "ipfs://...",
+	collectionURI:"",
+	collectionName: "Colection Name",
+	description:"collection description",
+	creatorName: "",
+	price: 0,
+	isFree: true,
+	createdByEDPON: false,
+}
+
+const mockCreator = {
+    wallet : "0xC1bd4Aa0a9ca600FaF690ae4aB67F15805d8b3A1",
+    name: "0xDMG",
+    creatorId: "",
+    artCollectionsId: []
+}
